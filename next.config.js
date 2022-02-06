3bf5dd355e465b73ea3d823e4648bab368ebc312
MaxHiit <pro.maxcode@gmail.com>
2022-02-06 14:42:43 +0100

    setup config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	compilerOptions: {
+		baseUrl: '.',
+		paths: {
+			'@components/': ['components/'],
+			'@config/': ['config/'],
+			'@api/': ['api/'],
+			'@lib/': ['lib/'],
+			'@styles/': ['styles/'],
+		},
+	},
+	include: ['*/.js'],
+	exclude: ['node_modules', '.next'],
 }
