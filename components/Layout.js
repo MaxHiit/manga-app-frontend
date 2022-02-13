45cc77696653c24bc564d74b2958884cd8cbc9b6
MaxHiit <pro.maxcode@gmail.com>
2022-02-13 20:36:22 +0100

    style: add font style

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,29 @@
+import Head from 'next/head'
 import Sidebar from '@components/Sidebar'
 
 const Layout = ({ children }) => {
 	return (
-		<div className='grid-layout'>
-			<Sidebar />
-			<header className='container'>
-				<nav className='navbar'>
-					<ul className='navbar-list'>
-						<li className='navbar-item navbar-item--logo '>MANGA-APP</li>
-						<li className='navbar-item'>Log in / Sign Up</li>
-					</ul>
-				</nav>
-			</header>
+		<>
+			<Head>
+				<link
+					href='https://api.fontshare.com/css?f[]=bevellier@400,500,600,700&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
+			<div className='grid-layout'>
+				<Sidebar />
+				<header className='container'>
+					<nav className='navbar'>
+						<ul className='navbar-list'>
+							<li className='navbar-item navbar-item--logo '>MANGA-APP</li>
+							<li className='navbar-item'>Log in / Sign Up</li>
+						</ul>
+					</nav>
+				</header>
 
-			<main className='main container'>{children}</main>
-		</div>
+				<main className='main container'>{children}</main>
+			</div>
+		</>
 	)
 }
 
